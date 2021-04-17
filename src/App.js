9e90072eb5006b7dd57be39f1c114899d691ad7f
Tigran Galstyan <galstyanctigran97@gmail.com>
2021-04-17 10:12:19 +0400

    Add material icons.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Typography from "@material-ui/core/Typography";
+import { Book } from "@material-ui/icons";
+import { green } from "@material-ui/core/colors";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import Icon from "@material-ui/core/Icon";
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
+}
 
 function App() {
   return (
@@ -13,6 +25,10 @@ function App() {
         <Typography variant="h4" gutterBottom>
           Testing Roboto Font.
         </Typography>
+        <HomeIcon color="primary" />
+        <HomeIcon style={{ color: green[500] }} fontSize="large" />
+        <Icon style={{ fontSize: 30 }}>add_circle</Icon>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +36,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Book />
         </a>
       </header>
     </div>
